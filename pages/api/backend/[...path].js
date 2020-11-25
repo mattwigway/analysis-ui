a8f5f150346f60e02903e0b9436aba545a3e558f
Matthew Wigginton Conway <matt@indicatrix.org>
2020-11-24 19:02:54 -0500

    feat(backend): proxy backend to avoid cors issues

---
+++ pages/api/backend/[...path].js
@@ -0,0 +1,41 @@
+/**
+ * Proxy requests to the backend in dev mode.
+ */
+
+import {createProxyMiddleware} from 'http-proxy-middleware'
+
+const dev = process.env.NODE_ENV == 'development'
+const proxy = dev
+  ? createProxyMiddleware({
+      target: process.env.NEXT_PUBLIC_BACKEND_PROXY_URL,
+      changeOrigin: true,
+      pathRewrite: {'^/api/backend': ''}
+    })
+  : null
+
+export const config = {
+  api: {
+    bodyParser: false // don't try to parse the body, id could be some binary blob
+  }
+}
+
+// https://nextjs.org/docs/api-routes/api-middlewares
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) return reject(result)
+      else return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  if (dev) {
+    await runMiddleware(req, res, proxy)
+    res.end()
+  } else {
+    res.status(400) // since this request came in production, don't give any information other than a 400
+  }
+}
+
+export default handler
